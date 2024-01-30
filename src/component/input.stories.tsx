820ccb8dde1cf8fe1a509f3a781ca9f8c09d0389
JuliaNezhel <yulyazelikova@gmail.com>
2024-01-30 21:11:49 +0300

    test b

--- src/component/input.stories.tsx
+++ src/component/input.stories.tsx
@@ -1,33 +1,35 @@
-import { action } from '@storybook/addon-actions';
-import { useRef, useState } from 'react';
+import {action} from '@storybook/addon-actions';
+import {useRef, useState} from 'react';
 import React from "react";
 
 export default {
-    title: 'imput',
+  title: 'imput',
 
 }
 
-export const UncontrolledInput = () => (<input />)
+export const UncontrolledInput = () => (<input/>)
 
 export const TrackVAlueUncontrolledInput = () => {
-    const [value, setValue] = useState('')
-    return (<>
-        <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const actualValue = e.currentTarget.value
-            setValue(actualValue)
-        }} /> value this: {value}
-    </>)
+  const [value, setValue] = useState('')
+  return (<>
+    <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+      const actualValue = e.currentTarget.value
+      setValue(actualValue)
+    }}/> value this: {value}
+  </>)
 }
 
 export const GetValueUncontrolledInput = () => {
-    const [value, setValue] = useState('')
-    const inputRef = useRef<HTMLInputElement>(null)
+  const [value, setValue] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
-    const save = () => {
-        const el = inputRef.current as HTMLInputElement;
-        setValue(el.value)
-    }
-    return (<>
-        <input ref={inputRef} /> <button onClick={save}>+</button> value save:  {value}
-    </>)
+  const save = () => {
+    const el = inputRef.current as HTMLInputElement;
+    setValue(el.value)
+  }
+  return (<>
+    <input ref={inputRef}/>
+    <button onClick={save}>+</button>
+    value save: {value}
+  </>)
 } 
